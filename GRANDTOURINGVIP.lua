6ea4a1b344255f2564eec08cb0f369ba93e9ad15
Xherry <cyhcherry@outlook.com>
2024-07-21 14:03:34 +0800

    update

--- GRANDTOURINGVIP.lua
+++ GRANDTOURINGVIP.lua
@@ -6,19 +6,6 @@
 ╚██████╔╝██║  ██║██║  ██║██║ ╚████║██████╔╝   ██║   ╚██████╔╝╚██████╔╝██║  ██║██║██║ ╚████║╚██████╔╝ ╚████╔╝ ██║██║     
  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝    ╚═╝    ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝      
 STRING ERROR~~~]]
-if async_http.have_access() then
-    local ms = {"lib.GTSCRIPTS.V", "lib.GTSCRIPTS.G", "lib.GTSCRIPTS.GTC.logo.GLogo"}
-    for _, m in ipairs(ms) do
-        if not package.loaded[m] then
-            package.loaded[m] = require(m)
-        end
-    end
-else
-    util.toast("[GRANDTOURINGVIP]\n选中禁止访问互联网时,GTLua将不可用")
-    util.stop_script()
-end
-
-
 local ms = {"lib.GTSCRIPTS.V","lib.GTSCRIPTS.G","lib.GTSCRIPTS.GTA.list"}
 for _, m in ipairs(ms) do
 	if not package.loaded[m] then
